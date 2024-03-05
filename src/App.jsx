543d7bd9fe1fd6f5da57fd835f723c8c56f2b747
Giovanni Masala <giovannimasala@Giovannis-MacBook-Pro.local>
2024-03-05 11:56:23 +0100

    added flowbite a react UI platform as a dependency for this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import react  from 'react'
+import React  from 'react'
+import { Button } from 'flowbite-react';
 
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+        <h1 className="text-3xl font-bold underline">
+          Ja<span className='decoration-[#6239ee]'>mmm</span>ing 
+        </h1>
+        <Button>Click me</Button>
+  </div>
+
   )
 }
 
